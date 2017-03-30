ff8fdac6171b40cc6cd5cd71581ffcd2ab5cddd6
Laimonas Simutis <lsimutis@mediafly.com>
2017-03-30 05:59:03 -0500

    basic console app structure

--- Filesplit.ConsoleApp/Program.cs
+++ Filesplit.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Filesplit.Services;
 
 namespace Filesplit.ConsoleApp
 {
@@ -6,7 +8,24 @@ namespace Filesplit.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create the service
+            var service = new RecordService();
+
+            // read in 3 files
+            foreach(var f in args)
+            {
+                var content = File.ReadAllText(f);
+
+                // pass the contents to the service
+                service.Add(content);
+            }
+
+            // three outputs
+            Console.WriteLine("Ouput 1, sorted by gender:");
+
+            Console.WriteLine("Ouput 2, sorted by DOB (ascending):");
+
+            Console.WriteLine("Ouput 3, sorted by last name (descending):");
         }
     }
 }
